411a80f22b56d3dadac87194ea28e20444383c50
Harisan02 <531493@edu.rocmn.nl>
2020-02-21 13:15:03 +0100

    hoi
    Alle tools.

---
+++ view/index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./style/style.css">
+    </head>
+    <body>
+        <?php include_once ("header.php"); ?>
+    </body>
+</html>
---
+++ view/navbar.php
@@ -0,0 +1,20 @@
+
+<!-- Creating The Navbar -->
+
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+   <div class="navbar-header">
+    <a class="navbar-brand" href="/">
+      <div class="logo-image">
+            <img src="../assets/mvm.png">
+      </div>
+    </a>
+    </div>
+    
+   <div class="collapse navbar-collapse" id="menu">
+      <ul class="nav navbar-nav">
+        <li><a href="?">Home</a></li>
+        <li><a href="?">Home</a></li>
+        <li><a href="?">Home</a></li>
+      </ul>
+   </div>
+</nav>
