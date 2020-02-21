2e7540fa1536e5395001a0bd5ec36d3fa2ba5377
Harisan02 <531493@edu.rocmn.nl>
2020-02-21 13:23:39 +0100

    Update navbar.php

--- view/navbar.php
+++ view/navbar.php
@@ -1,20 +1,15 @@
 
 <!-- Creating The Navbar -->
 
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-   <div class="navbar-header">
-    <a class="navbar-brand" href="/">
-      <div class="logo-image">
-            <img src="../assets/mvm.png">
-      </div>
-    </a>
-    </div>
-    
-   <div class="collapse navbar-collapse" id="menu">
-      <ul class="nav navbar-nav">
-        <li><a href="?">Home</a></li>
-        <li><a href="?">Home</a></li>
-        <li><a href="?">Home</a></li>
-      </ul>
-   </div>
-</nav>
+<div class="container">
+<div class="logo">
+  <img src="../assets/mvm.png" alt="" width="130"/>
+  </div>
+<nav>
+  <ul>
+  <li>Home</li>
+  <li>Alle Producten</li>
+  <li>Contact</li>
+  </ul>
+  </nav>
+</div>
