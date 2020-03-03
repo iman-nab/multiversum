ac02106105f78474ec7d78687a6e3f7c0d00858f
Harisan02 <531493@edu.rocmn.nl>
2020-03-03 09:05:43 +0100

    gizem
    -

--- view/navbar.php
+++ view/navbar.php
@@ -2,16 +2,34 @@
 
 <!-- Creating The Navbar -->
 
-  <div class="container">
-  <div class="logo">
-    <img src="../assets/mvm.png" alt="" width="130"/>
-  </div>
-  
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>Alle Producten</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-  </div>
+<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                   <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
